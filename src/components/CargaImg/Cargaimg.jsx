e6888b521a1f12e12e99ac419382f1af5ec8be7b
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2023-07-07 23:11:56 -0600

    Cambi boton

--- src/components/CargaImg/Cargaimg.jsx
+++ src/components/CargaImg/Cargaimg.jsx
@@ -69,7 +69,7 @@ export function Cargaimg(cod) {
                 className="btn btn-primary px-3 py-2"
                 type="button"
               >
-                Cargar
+                Limpiar
               </button>
             </div>
             {saveChanges ? (
